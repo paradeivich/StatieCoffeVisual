bc6ff2babc11228d73e8aefad8ba16132d1cd18c
Dumitru Ghilescu <dumitru.ghilescu@student.usv.ro>
2023-05-15 22:42:57 +0300

    80%

--- StatieCoffeVisual/StatieCoffeVisual.cpp
+++ StatieCoffeVisual/StatieCoffeVisual.cpp
@@ -15,19 +15,57 @@ using namespace std;
 class PreparareCoffe
 {
 public:
-    void preparareBautura(Coffe &coffe, int zahar)
+    void preparareBautura(Coffe &coffe, int sugar)
     {
         cout << endl << "Se prepara bautura: " << coffe.getName() << endl;
         cout << coffe.getCoffe() << "ml coffe: ";
         cout << coffe.getGramsOfMilk() << "ml milk: ";
-        cout << zahar * 10 << "mg sugar: " << endl;
+        cout << sugar * 10 << "mg sugar: " << endl;
         cout << "\n";
     }
-    void makeExpresso(int zahar)
+    void makeLatteCoffe(int sugar)
+    {
+        LatteCoffe coffe;
+        preparareBautura(coffe, sugar);
+    };
+
+    void makeExpresso(int sugar)
     {
         ExpressoCoffe coffe;
-        preparareBautura(coffe, zahar);
+        preparareBautura(coffe, sugar);
     };
+
+    void makeShortExpresso(int sugar)
+    {
+        ShortExpresso coffe;
+        preparareBautura(coffe, sugar);
+    };
+    void makeLongExpresso(int sugar)
+    {
+        LongExpresso coffe;
+        preparareBautura(coffe, sugar);
+    };
+    void makeCappuccino(int sugar)
+    {
+        Cappucino coffe;
+        preparareBautura(coffe, sugar);
+    };
+    void makeArabicCoffe(int sugar)
+    {
+        ArabicCoffe coffe;
+        preparareBautura(coffe, sugar);
+    };
+    void makeHotChocolatte(int sugar)
+    {
+        HotChocolatte coffe;
+        preparareBautura(coffe, sugar);
+    };
+    void makeLatteCream(int sugar)
+    {
+        LatteCream coffe;
+        preparareBautura(coffe, sugar);
+    };
+    
 
 };
 void MenuCoffe()
@@ -40,34 +78,125 @@ void MenuCoffe()
     cout << "9. Exit" << endl;
     cout << endl << "Alegeti optiunea: ";
 }
+int readMoney(int drinkCost)
+{
+    int sum;
+    cout << endl << "Pret: " << drinkCost << "Introduceti suma: .";
+    cin >> sum;
+    return sum;
+}
+bool costIsValid(int userInput, int cost)
+{
+    if (userInput < cost)
+    {
+        cout << endl << "Nu ati introdus suma necesara! ";
+        return false;
+    }
+    return true;
+}
+void proceseazaRestul(int userInput, int cost)
+{
+    cout << endl << "Rest: " << (userInput - cost) << "lei";
+}
+int readSugar()
+{
+    int level;
+    cout << "Selectati NIVELUL de zahar dorit (1-5)";
+    cin >> level;
+    return level;
+}
+
 
 int main()
 {
+    PreparareCoffe preparareCoffe;
+
     MenuCoffe();
     int opt;
     do
     {
         cin >> opt;
+        int userInput, nivelZahar;
         switch (opt)
         {
         case 1:
+            userInput = readMoney(5);
+            if (costIsValid(userInput, 5))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeExpresso(nivelZahar);
+                proceseazaRestul(userInput, 5);
+            }
             break;
         case 2:
+            userInput = readMoney(9);
+            if (costIsValid(userInput, 9))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeShortExpresso(nivelZahar);
+                proceseazaRestul(userInput, 9);
+            }
             break;
         case 3:
+            userInput = readMoney(10);
+            if (costIsValid(userInput, 10))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeLongExpresso(nivelZahar);
+                proceseazaRestul(userInput, 10);
+            }
             break;
         case 4:
+            userInput = readMoney(8);
+            if (costIsValid(userInput, 8))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeLatteCoffe(nivelZahar);
+                proceseazaRestul(userInput, 8);
+            }
             break;
         case 5:
+            userInput = readMoney(4);
+            if (costIsValid(userInput, 4))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeCappuccino(nivelZahar);
+                proceseazaRestul(userInput, 4);
+            }
             break;
         case 6:
+            userInput = readMoney(12);
+            if (costIsValid(userInput, 12))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeArabicCoffe(nivelZahar);
+                proceseazaRestul(userInput, 12);
+            }
             break;
         case 7:
+            userInput = readMoney(9);
+            if (costIsValid(userInput, 9))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeHotChocolatte(nivelZahar);
+                proceseazaRestul(userInput, 9);
+            }
             break;
         case 8:
+            userInput = readMoney(6);
+            if (costIsValid(userInput, 6))
+            {
+                nivelZahar = readSugar();
+                preparareCoffe.makeLatteCream(nivelZahar);
+                proceseazaRestul(userInput, 6);
+            }
             break;
+            
         case 9:
-            break;
+            return 0;
+        default:
+            cout << "Ati ales o optiune gresita! Reintroduceti alta optiune valida:" << endl << endl;
+            MenuCoffe();
         }
         
     }
